a01d7af7a2095e1ae58c50a9a5b0e16bfd785233
Mert Ekren <mert.ekren@analog.com>
2024-12-23 10:24:10 +0300

    boards: max32657evkit/max32657/ns: flash tfm_merged.hex on default
    This commit provides flashing tfm_merged.hex (includes
    mcuboot+tfm+ns_image) on default.
    
    Signed-off-by: Mert Ekren <mert.ekren@analog.com>

--- boards/adi/max32657evkit/board.cmake
+++ boards/adi/max32657evkit/board.cmake
@@ -1,6 +1,10 @@
 # Copyright (c) 2024 Analog Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+if(CONFIG_BOARD_MAX32657EVKIT_MAX32657_NS)
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
+endif()
+
 board_runner_args(openocd --cmd-pre-init "source [find interface/jlink.cfg]")
 board_runner_args(openocd --cmd-pre-init "source [find target/max32657.cfg]")
 
