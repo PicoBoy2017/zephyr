12e53a5f705e8ac9eabd04fc57700805f2a8ec56
Sadik Ozer <sadik.ozer@analog.com>
2025-01-27 14:41:41 +0300

    boards: adi: Centralize openocd debugger configuration
    Add a commont openocd configuration files for MAX32 MCUs to simplify
    maintanence of them. After this addition it will be simply to add/remove
    command to the MAX32 MCUs
    
    Signed-off-by: Sadik Ozer <sadik.ozer@analog.com>

--- boards/adi/max32655evkit/board.cmake
+++ boards/adi/max32655evkit/board.cmake
@@ -2,14 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 if(CONFIG_BOARD_MAX32655EVKIT_MAX32655_M4)
-  board_runner_args(openocd --cmd-pre-init "source [find interface/cmsis-dap.cfg]")
-  board_runner_args(openocd --cmd-pre-init "source [find target/max32655.cfg]")
-  board_runner_args(openocd --cmd-pre-init "allow_low_pwr_dbg")
   board_runner_args(jlink "--device=MAX32655" "--reset-after-load")
-elseif(CONFIG_BOARD_MAX32655EVKIT_MAX32655_RV32)
-  board_runner_args(openocd --cmd-pre-init "source [find interface/ftdi/olimex-arm-usb-ocd-h.cfg]")
-  board_runner_args(openocd --cmd-pre-init "source [find target/max32655_riscv.cfg]")
 endif()
 
+include(${ZEPHYR_BASE}/boards/common/openocd-adi-max32.boards.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
--- boards/adi/max32672fthr/board.cmake
+++ boards/adi/max32672fthr/board.cmake
@@ -1,8 +1,5 @@
 # Copyright (c) 2024 Analog Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(openocd --cmd-pre-init "source [find interface/cmsis-dap.cfg]")
-board_runner_args(openocd --cmd-pre-init "source [find target/max32672.cfg]")
-board_runner_args(openocd --cmd-pre-init "allow_low_pwr_dbg")
-
+include(${ZEPHYR_BASE}/boards/common/openocd-adi-max32.boards.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
